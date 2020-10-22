59c663688569a8d141b862114751179aef405923
JanKulbinski <jan.kulbinski2@gmail.com>
2020-10-22 10:00:52 +0200

    monitors returns path to html

--- backend/blueprints/monitors.py
+++ backend/blueprints/monitors.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, request, session, jsonify, abort, make_response, send_from_directory
 from requests_html import HTMLSession
 from pywebcopy import WebPage, config
-
+import re
 monitors = Blueprint('monitors', __name__, url_prefix='/monitors')
-from app import mysql, PATH_TO_SAVE_STATIC
+from app import mysql, PATH_TO_SAVE_STATIC, SERVER_URL
 
-
+# gdy folder juz istnieje to strona nie jest sciagana, bedzie trzeba ja sciagac do temp
 @monitors.route("/page-to-monitor", methods=['GET'])
 def save_whole_page():
     adress = request.args.get('adress')
@@ -14,16 +14,14 @@ def save_whole_page():
     wp = WebPage()
     wp.get(adress)
 
-    # start the saving process
     wp.save_complete()
     timeout = 1
-    # join the sub threads
-    for t in wp._threads:
+    for t in wp._threads: # blocks the calling thread until the thread whose join() method is called is terminated.
         if t.is_alive():
            t.join(timeout)
 
-    # location of the html file written 
-    return jsonify({'location': wp.file_path})
+    webpage_path = SERVER_URL + re.search("static.*", wp.file_path).group().replace('//', '/')
+    return jsonify({'location': webpage_path})  
 
 
 def download_html(adress):
